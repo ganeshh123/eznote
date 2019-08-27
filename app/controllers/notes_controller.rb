0b0e935d63f727c6823a596078f55a0e7c91fe98
Ganesh <43777439+ganeshh123@users.noreply.github.com>
2019-08-27 22:40:22 +0100

    Generated Notes model and Controller

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,32 @@
+class NotesController < ApplicationController
+
+    def index
+
+    end
+
+    def show
+
+    end
+
+    def new
+
+    end
+
+    def create
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
+    private
+
+    def note_params
+
+    end
+end
